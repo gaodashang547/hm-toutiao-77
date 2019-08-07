f69271bc76f6c14ac19c68e8e7987883cf745b52
feng <547190485@qq.com>
2019-08-07 15:40:20 +0800

    home

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,11 +1,92 @@
 <template>
-    <div id="container">articles</div>
+  <div id="container">
+    <el-card>
+      <div slot="header">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-form label-width="90px" size="small">
+        <el-form-item label="状态：">
+          <el-radio-group v-model="a.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <el-select v-model="a.channel_id" placeholder="请选择">
+            <el-option
+              v-for="item in channelOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期：">
+          <el-date-picker
+            v-model="dateArr"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">筛选</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  <el-card>
+    <div slot="header">根据条件查询0条数据：</div>
+    <!-- 表格组件 -->
+     <el-table :data="article">
+       <el-table-column prop="img" label="封面" > </el-table-column>
+    </el-table>
+    <!-- 分页组件 -->
+    <div style="text-align:center; margin-top:30px;">
+  <el-pagination
+       background
+       layout="prev, pager, next"
+       :total="1000"
+       >
+    </el-pagination>
+
+    </div>
+
+  </el-card>
+
+   </div>
 </template>
 <script>
-// export default {
-//   // created () {
-//   //   this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/articles').then(res => console.log(res.data))
-//   // }
+export default {
+  // created () {
+  //   this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/articles').then(res => console.log(res.data))
+  // }
+  data () {
+    return {
+      // 筛选项表单数据  提交给后台参数
+      a: {
+        status: null,
+        channel_id: null
+      },
+      // 频道下拉选项数据
+      channelOptions: [
+      ],
+      // 日期数据
+      dateArr: [],
+      article: []
 
-// }
+    }
+  }
+}
 </script>
+<style scoped  lang="less"  >
+   .el-card{
+     margin-top:40px;
+   }
+</style>
