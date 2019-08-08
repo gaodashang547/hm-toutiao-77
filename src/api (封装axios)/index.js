9f086868b7a267b9f3c932f0deaa29f033c91477
feng <547190485@qq.com>
2019-08-08 19:23:28 +0800

    文章管理

--- src/api (封装axios)/index.js
+++ src/api (封装axios)/index.js
@@ -2,9 +2,25 @@
 import axios from 'axios'
 import store from '@/store（本地存储）'
 import router from '@/router（路由）'
-
+import JSONBig from 'json-bigint'
+// 下载并引入 json-bigint 这个第三方包并进行配置通过这个第三方包来解决
+// 超过js的最大安全值 也就是说来解决ID值不匹配的问题
 // 进行配置
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+axios.defaults.transformResponse = [
+  (data) => {
+    // data  此时是后台返回给我们的数据
+    // data 如果后台没有返回数据 此时data是null
+    // JSONBig.parse(null) 报错 阻止程序运行
+    // 在这里 通过try{}catch(){}  来捕获异常  如果 后台返回的data是个null 并且没有通过try{}catch(){}来进行捕获异常
+    // 则上述中格式转换会报错 阻止程序的继续往下执行 若是捕获了异常则直接接返回一个原有data 即可
+    try {
+      return JSONBig.parse(data)
+    } catch (e) {
+      return data
+    }
+  }
+]
 // 只会执行一次
 // axios.defaults.headers = {
 //   Authorization: `Bearer ${store.getUser().token}`
@@ -42,6 +58,12 @@ axios.interceptors.response.use((res) => {
     router.push('/login')
   }
   return Promise.reject(err)
+  // promise。reject（） 这是返回一个失败的promise对象
+  // Promise.rslove() 这是返回一个成功的promise对象
+  // reject 是在失败的时候应该调用的回调函数
+  // reslove 是在成功的时候应该调用的回调函数
+  // promise.then（成功）
+  // promise.catch（失败）
 })
 
 // 进行导出
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -18,7 +18,7 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道：">
-          <el-select v-model="reqParams.channel_id" placeholder="请选择">
+          <el-select v-model="reqParams.channel_id" placeholder="请选择" clearable>
             <el-option
               v-for="item in channelOptions"
               :key="item.id"
@@ -27,6 +27,9 @@
             ></el-option>
           </el-select>
         </el-form-item>
+        <!-- 在给后台传递日期的时候 是以TTue Aug 13 2019 00:00:00 GMT+0800 (中国标准时间)
+        这种标准格式向后台传递的 而后台的数据接口却无法解读 所以element-ui 则提供了一种属性
+        value-format="yyyy-MM-dd" 可以将标准格式的时间转化为年月日的格式-->
         <el-form-item label="日期：">
           <el-date-picker
             v-model="dateArr"
@@ -34,10 +37,13 @@
             range-separator="至"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
+            @change="changeDate"
+            value-format="yyyy-MM-dd"
           ></el-date-picker>
+
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">筛选</el-button>
+          <el-button type="primary" @click="shuaixuan">筛选</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -58,19 +64,23 @@
           <el-table-column label="标题" prop="title"></el-table-column>
           <el-table-column label="状态">
             <template slot-scope="scope">
+              <!-- 作用于插槽 一般都是用template进行包裹  给其标签内部设置slot-scope="scope"
+                 通过scope.row 来获取想要获取到的值
+                -->
               <el-tag v-if="scope.row.status===0" type="info">草稿</el-tag>
               <el-tag v-if="scope.row.status===1" type="">待审核</el-tag>
               <el-tag v-if="scope.row.status===2" type="success">审核通过</el-tag>
               <el-tag v-if="scope.row.status===3" type="warning">审核失败</el-tag>
               <el-tag v-if="scope.row.status===4" type="danger">删除</el-tag>
-            </template>1
+            </template>
 
           </el-table-column>
           <el-table-column label="发布时间" prop="pubdate"></el-table-column>
           <el-table-column label="操作" prop="img">
            <template slot-scope="scope">
-             <el-button type="primary" icon="el-icon-edit" circle></el-button>
-             <el-button type="danger" icon="el-icon-delete" @click="del(scope.row.id)" ></el-button>
+             <!-- 给跳转图标设置点击事件并将参数（也就是需要跳转的文章的id）传递进去 -->
+             <el-button type="primary" icon="el-icon-edit" @click="tiao(scope.row.id)" circle></el-button>
+             <el-button type="danger" icon="el-icon-delete" @click="del(scope.row.id)" circle ></el-button>
            </template>
            </el-table-column>
        </el-table>
@@ -82,6 +92,7 @@
        v-bind:page-size="reqParams.per_page"
        :total="total"
        @current-change='changePager' >
+       <!-- current-change 这是element-ui 中提供的 事件 -->
        <!-- ：total='1000' 这是指定了总条数 -->
        <!-- 默认一页显示10条 ：page-size='reqParams.per_page' 这样可以不用写死 与数据data中形成了一种相对应的形式 -->
     </el-pagination>
@@ -116,6 +127,15 @@ export default {
 
     }
   },
+  // computed 计算属性使用的场景：当你需要一个新的数据 需要依赖data中的数据
+  // watch 侦听器的使用场景：当你需要监听一个属性的值的变化时，去做一些开销较大的操作（异步操作）
+  watch: {
+    'reqParams.channel_id': function (newVal, oldVal) {
+      if (newVal === '') {
+        this.reqParams.channel_id = null
+      }
+    }
+  },
   // 获取频道下拉选项数据
   created: function () {
     this.getChannelOptions()
@@ -123,12 +143,57 @@ export default {
     this.getArtciles()
   },
   methods: {
+    // 点击图标删除文章
+    del (id) {
+      // 弹出确认框 点击确认后发 送删除请求  相应成功后涮新文章列表
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const abc = await this.$http.delete(`/articles/${id}`)
+        console.log(abc)
+
+        this.$message.success('您已删除文章成功')
+        this.getArtciles()
+      }).catch(() => {})
+      // 在点击删除按钮后通过控制台发现我们发送的初始id值和后台通过json.parse转换后
+      // 返回给我们通过axios获取的id值对应不上，Id的数值超出了js的最大安全值
+      // 最大安全值：
+      //  Number.MAX_SAFE_INTEGER   如果超出了最大安全值的计算和转换 都会出现误差 并报错
+    },
+    // 点击编辑按钮进行跳转 在跳转的同时 需要将文章的id一同传递获取所以要在地址后面 进行字符串的拼接
+    // 将文章的id 拼接在地址的后面
+    tiao (id) {
+      this.$router.push('/publish?id=' + id)
+    },
+    //   筛选时日期的控制
+    changeDate (dateArr) {
+      if (dateArr) {
+        this.reqParams.end_pubdate = dateArr[1]
+        this.reqParams.begin_pubdate = dateArr[0]
+      } else {
+        this.reqParams.begin_pubdate = null
+        this.reqParams.end_pubdate = null
+      }
+
+      console.log(dateArr)
+    },
+    // 根据条件进行筛选从新渲染页面
+    shuaixuan () {
+      this.reqParams.page = 1
+      this.getArtciles()
+    },
     // 改变分页时间所对应的的函数
     changePager (newPage) {
       this.reqParams.page = newPage
       this.getArtciles()
     },
-
+    // 在发送请求时 使用asyn 和使用await的条件是
+    //  1 必须是是用promise对象的时候
+    //  2 给返回promise的函数前 加上await关键字，用来直接获取到成功的结果
+    //  3 但是是用await这个关键字的函数之外 必须是加上asyn的这个关键字来进行曲修饰
+    //
     async getChannelOptions () {
       const { data: { data } } = await this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/channels')
       this.channelOptions = data.channels
@@ -141,6 +206,8 @@ export default {
       this.articles = data.results
       // 这是文章的总数据（有多少条）
       this.total = data.total_count
+    // 使用作用于插槽 results之外的数据 articles掺入了表格组件，帮你做了遍历，每一项数据插槽 row=“每一项数据”
+    // 所以使用每一项数据 其实就是组件内部的数据  通过scope.row 来获取想要获取到的数据
     }
   }
 }
